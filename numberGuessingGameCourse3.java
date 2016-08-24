610c5ff4c1096b910c30b71e1ecc391ff98fd5f0
Hydrogen100 <180325@hkis.edu.hk>
2016-08-24 23:58:36 +0800

    numberGuessingGameAddDetails
    Adding details now
    Currently trying to reject inputs that contain a 0

--- numberGuessingGameCourse3.java
+++ numberGuessingGameCourse3.java
@@ -11,29 +11,40 @@ public static void main(String args[]){
 	System.out.println("Hello, this program is a number guessing game based on MasterMind.");
 	System.out.println("The goal of the game is to guess the numbers in the correct order");
 	System.out.println("First, input four numbers with no spaces in between from 1-9");
-	System.out.println("Then, proceed with guessing, and 0s and 1s will be returned");
-	System.out.println("A 0 means the number exists in the code, but is at the wrong location");
-	System.out.println("On the other hand, a 1 means the number exists in the code, and is at the right location");
 	System.out.println("Please input your four numbers now");
 	Scanner input=new Scanner(System.in);
-//reject the input if it is not 4 numbers
-//reject if the input contains a 0
-	String fourNumbers=input.next();
-//next 9 lines can be simplified to 5 only
-	char one=fourNumbers.charAt(0);
-	char two=fourNumbers.charAt(1);
-	char three=fourNumbers.charAt(2);
-	char four=fourNumbers.charAt(3);
+    //reject the input if it is not 4 numbers
+//REJECT IF THE INPUT CONTAINS a 0
+	boolean wrongString=true;
+	String fourNumbers="a";
 	char[] answerArray=new char[4];
-	answerArray[0]=one;
-	answerArray[1]=two;
-	answerArray[2]=three;
-	answerArray[3]=four;
+	while(wrongString){
+	fourNumbers=input.next();
+	answerArray[0]=fourNumbers.charAt(0);
+	answerArray[1]=fourNumbers.charAt(1);
+	answerArray[2]=fourNumbers.charAt(2);
+	answerArray[3]=fourNumbers.charAt(3);
+	int countForZero=0;
+	for(int a=0;a<4;a++){
+		if(answerArray[a]!=0){
+			countForZero++;
+		}
+	}
+	if(fourNumbers.length()==4&&countForZero==4){
+		wrongString=false;
+	}else{
+	System.out.println("Please input again");
+	}
+	
+	}
+	
+	
 	//ask user to start guessing
-	//maybe count the number of tries required?
+    //maybe count the number of tries required?
 	System.out.println("Please proceed with guessing now");
 	boolean gameLose=true;
 	numberGuessed[] guessArray=new numberGuessed[4];
+	int tries=1;
 	while(gameLose){
 		String guessedNumber=input.next();
 		numberGuessed guessOne=new numberGuessed(0);
@@ -89,8 +100,9 @@ public static void main(String args[]){
 	}
 		if(countCorrectSpot==4){
 			gameLose=false;
-			System.out.println("Congratulations, you won the game!");
+			System.out.println("Congratulations, you won the game! Completed with "+tries+" tries");
 		}else{
+			tries++;
 			System.out.println("Number in correct spots: "+countCorrectSpot);
 			System.out.println("Number of correct guesses but in wrong spots: "+ countCorrectWrongSpot);
 			
